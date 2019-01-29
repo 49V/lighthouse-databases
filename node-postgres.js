84e7466c3cba6d48e3d01700c3f07b2312f268a0
Moshe Lawlor <moshelawlor@gmail.com>
2019-01-29 17:58:50 +0000

    Add formatDate() function to formate date in a properly printable form

--- node-postgres.js
+++ node-postgres.js
@@ -32,11 +32,20 @@ client.connect((err) => {
 
 function displayResult (rows) {
   console.log(`Found ${rows.length} person(s) by the name of ${process.argv[2]}`);
-  
+  let count = 1;  
+
   rows.forEach(function(person) {
-    console.log(`${person.id}: ${person.first_name} ${person.last_name}, born '${person.birthdate}'`);
+    const formattedDate = formatDate(person.birthdate)
+    console.log(`${count}: ${person.first_name} ${person.last_name}, born '${formattedDate}'`);
+    count++;
   });
+}
 
-
+function formatDate (date) {
+  const newDate = new Date(date);
+  const year = newDate.getFullYear();
+  const month = (newDate.getMonth() < 9) ? '0' + (newDate.getMonth() + 1): newDate.getMonth() + 1;
+  const day = (newDate.getDate() < 10) ? '0' + newDate.getDate() : newDate.getDate();
+  return `${year}-${month}-${day}`
 }
 
