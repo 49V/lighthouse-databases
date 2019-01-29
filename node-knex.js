4f029a740272236f3e04791a704c2873996c354a
Moshe Lawlor <moshelawlor@gmail.com>
2019-01-29 20:36:07 +0000

    Create node-knex.js to construct queries using Knex

--- node-knex.js
+++ node-knex.js
@@ -1,1 +1,25 @@
 const helpers = require('./helpers');
+const settings = require('./settings')
+const name = process.argv[2];
+
+const knex = require('knex')({
+  client: 'pg',
+  connection: {
+    host     : settings.hostname,
+    user     : settings.user,
+    password : settings.password,
+    database : settings.database,
+    port     : settings.port,
+    ssl      : settings.ssl
+  }
+});
+
+knex.select('*')
+  .from('famous_people')
+  .where({first_name: name})
+  .orWhere({last_name: name})
+  .asCallback(function(error, rows) {
+    if (error) return console.error(error);
+      helpers.displayResult(rows); 
+    knex.destroy();
+  });
--- node-postgres.js
+++ node-postgres.js
@@ -2,7 +2,6 @@ const helpers = require("./helpers");
 const pg = require("pg");
 const settings = require("./settings"); // settings.json
 const name = process.argv.slice(2)
-console.log(name);
 
 const client = new pg.Client({
   user     : settings.user,
